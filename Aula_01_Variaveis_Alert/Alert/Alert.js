96b6d6e3da05ee6c7d5dd228caf45c93b47b0014
Robson Santana <www.robson.dev@gmail.com>
2024-09-26 15:46:32 -0300

    mudando o nome da pasta

---
+++ Aula_01_Variaveis_Alert/Alert/Alert.js
@@ -0,0 +1,5 @@
+// Alert é uma caixa de alerta que aparece na tela ao clicar no botão.
+
+alert("Ola , Robson") // Alerta de boas-vindas ao usuário 
+
+alert("Aprendendo JS") // Alerta de aprendizado ao usuário 
